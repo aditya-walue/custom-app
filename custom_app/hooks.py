8eb9c308e84c5c6b5b1125cebbaacbb8f8330d22
aditya-walue <Aditya@Walue.biz>
2026-06-18 11:32:04 +0530

    feat: configure and include cleaned v16-compliant fixtures

--- custom_app/hooks.py
+++ custom_app/hooks.py
@@ -7,7 +7,12 @@ app_license = "mit"
 
 # Apps
 # ------------------
-
+fixtures = [
+	{"dt": "Client Script"},
+    {"dt": "Server Script"},
+   {"dt": "Custom Field"}
+    
+]
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
